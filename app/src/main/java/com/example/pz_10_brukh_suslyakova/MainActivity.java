f352532f0f8f4ef77638a75377662ef0ad339962
CopperStone71 <bruhdmitr@yandex.ru>
2024-05-30 11:15:26 +0700

    first commit

---
+++ app/src/main/java/com/example/pz_10_brukh_suslyakova/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pz_10_brukh_suslyakova;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        Draw2D draw2D = new Draw2D(this);
+        setContentView(draw2D);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
